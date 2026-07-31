8fa2c21cda6064092b933b7a47e2660f411c0999
aziza0-crypto <azizasagynbekkyzy0.gmail.com>
2026-07-31 18:53:38 +0600

    DZ6 подключение данных Student к HTML

--- apps/nurai/views.py
+++ apps/nurai/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
 from .models import Student
 
 
 def index(request):
     students = Student.objects.all()
-    return render(request, "index.html", locals())
+
+    return render(request, "index.html", {
+        "students": students,
+        "test": "Я ИЗ VIEWS.PY"
+    })
